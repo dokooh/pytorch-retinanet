f1960db867a7f90bbd93946c8d4b85cbb3999c93
Nima Dokoohaki <nimadokoohaki@gmail.com>
2021-09-08 15:19:11 +0200

    Pretrained set to False
    Given issues with unpickling of the pretrained models in GCP

--- train.py
+++ train.py
@@ -77,7 +77,7 @@ def main(args=None):
     if parser.depth == 18:
         retinanet = model.resnet18(num_classes=dataset_train.num_classes(), pretrained=True)
     elif parser.depth == 28: #WideResnet
-        retinanet = model.wide_resnet28_10(num_classes=dataset_train.num_classes(), pretrained=True) #checking if we can use Totem pretrained WideResnet model           
+        retinanet = model.wide_resnet28_10(num_classes=dataset_train.num_classes(), pretrained=False) #checking if we can use Totem pretrained WideResnet model           
     elif parser.depth == 34:
         retinanet = model.resnet34(num_classes=dataset_train.num_classes(), pretrained=True)
     elif parser.depth == 50:
